620355e741eacf1b22a4f820cd022d82f6769ded
Pablo Bailador Pollo <pablobailador@gmail.com>
2023-09-22 09:39:44 +0200

    Install packages(tidyverse).

--- HANDS_ON_01.R
+++ HANDS_ON_01.R
@@ -2,3 +2,42 @@
 # NOMBRE :Pablo Bailador Pollo
 # EXP:22273714
 # TEMA:HANDS_ON_01
+
+
+# LOADING LIBS ------------------------------------------------------------
+
+install.packages("tidyverse")
+
+# LOADING DATA ------------------------------------------------------------
+
+
+# SHORTCUTS ---------------------------------------------------------------
+
+# CLEAN CONSOLE = CTRL + L
+# %>% = pipe generator = SHIFT + CTRL + M
+# NEW INDEX = SHIFT + CTRL + R
+# CTRL + ENTER = run line
+
+
+# GIT COMMANDS ------------------------------------------------------------
+
+# git status = info about a repo
+# git commit = Add a comment
+# git add . = Add the curent dir to the entire repo
+# git push -u origin main = send to the remote repo (Github)
+
+
+# CLI COMMANDS ------------------------------------------------------------
+
+# pwd = shows the current dir location
+# ls = list terminal
+# mkdir = create a dir
+# cd = change dir
+
+
+# BASIC INSTRUCTION ----------------------------------------------------------
+
+pablo<-8 # assigning values
+
+
+
